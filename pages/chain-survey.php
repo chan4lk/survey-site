fecdcfb612665c24728ea56be5ae0f1cd5116765
Chandima Ranaweera <chan4lk@gmail.com>
2018-07-24 23:08:50 +0530

    add content

--- pages/chain-survey.php
+++ pages/chain-survey.php
@@ -1,1 +1,88 @@
 <h1>Chain Survey</h1>
+<div class="row">
+    <video width="100%" height="440" controls>
+        <source src="public/videos/Video_4.mkv" type="video/mp4"> Your browser does not support the video tag.
+    </video>
+</div>
+
+<div class="row">
+    Fondue cauliflower cheese cheese slices. Blue castello stinking bishop cheese on toast cheese triangles cheeseburger halloumi
+    halloumi danish fontina. Edam cheese on toast roquefort jarlsberg ricotta cheesy feet gouda emmental. Jarlsberg babybel
+    lancashire cheeseburger stinking bishop. Smelly cheese rubber cheese goat. Who moved my cheese bavarian bergkase cheese
+    strings fromage babybel cheese and biscuits cow emmental. Cottage cheese paneer cheese strings melted cheese fromage
+    frais edam cottage cheese who moved my cheese. Bocconcini cheddar gouda stinking bishop taleggio cow port-salut. Brie
+    manchego mascarpone. Ricotta say cheese cheese slices fondue cheesecake hard cheese cheesy feet port-salut. Lancashire
+    parmesan macaroni cheese who moved my cheese cottage cheese red leicester cottage cheese port-salut. Squirty cheese bavarian
+    bergkase cream cheese. Gouda stilton cheese strings. Ricotta queso mozzarella hard cheese lancashire when the cheese
+    comes out everybody's happy camembert de normandie cheeseburger. Cauliflower cheese who moved my cheese cottage cheese
+    jarlsberg say cheese fromage frais who moved my cheese cheesecake. Ricotta. Fromage cheese strings the big cheese. Airedale
+    the big cheese who moved my cheese red leicester rubber cheese croque monsieur the big cheese port-salut. Gouda fondue
+    bavarian bergkase ricotta swiss smelly cheese halloumi macaroni cheese. Cut the cheese swiss say cheese cottage cheese
+    red leicester macaroni cheese.</div>
+<div class="row">
+    <h2>Instrument</h2>
+</div>
+<div class="row">
+    <div class="col-md-6">
+        <ul>
+            <li>Steel tape</li>
+            <li>Ranging Poles</li>
+            <li>Linen tape</li>
+        </ul>
+    </div>
+    <div class="col-md-6">
+        <img class="col-md-6 img-responsive" src="public/images/Image_6.jpg" />
+        <img class="col-md-6 img-responsive" src="public/images/Image_6.jpg" />
+    </div>
+</div>
+<div class="row">
+    <h2>Ranging</h2>
+</div>
+<div class="row">
+    Fondue cauliflower cheese cheese slices. Blue castello stinking bishop cheese on toast cheese triangles cheeseburger halloumi
+    halloumi danish fontina. Edam cheese on toast roquefort jarlsberg ricotta cheesy feet gouda emmental. Jarlsberg babybel
+    lancashire cheeseburger stinking bishop. Smelly cheese rubber cheese goat. Who moved my cheese bavarian bergkase cheese
+    strings fromage babybel cheese and biscuits cow emmental. Cottage cheese paneer cheese strings melted cheese fromage
+    frais edam cottage cheese who moved my cheese. Bocconcini cheddar gouda stinking bishop taleggio cow port-salut. Brie
+    manchego mascarpone. Ricotta say cheese cheese slices fondue cheesecake hard cheese cheesy feet port-salut. Lancashire
+    parmesan macaroni cheese who moved my cheese cottage cheese red leicester cottage cheese port-salut. Squirty cheese bavarian
+    bergkase cream cheese. Gouda stilton cheese strings. Ricotta queso mozzarella hard cheese lancashire when the cheese
+    comes out everybody's happy camembert de normandie cheeseburger. Cauliflower cheese who moved my cheese cottage cheese
+    jarlsberg say cheese fromage frais who moved my cheese cheesecake. Ricotta. Fromage cheese strings the big cheese. Airedale
+    the big cheese who moved my cheese red leicester rubber cheese croque monsieur the big cheese port-salut. Gouda fondue
+    bavarian bergkase ricotta swiss smelly cheese halloumi macaroni cheese. Cut the cheese swiss say cheese cottage cheese
+    red leicester macaroni cheese.</div>
+<div class="row">
+    <h2>Stepping Method</h2>
+</div>
+<div class="row">
+    Fondue cauliflower cheese cheese slices. Blue castello stinking bishop cheese on toast cheese triangles cheeseburger halloumi
+    halloumi danish fontina. Edam cheese on toast roquefort jarlsberg ricotta cheesy feet gouda emmental. Jarlsberg babybel
+    lancashire cheeseburger stinking bishop. Smelly cheese rubber cheese goat. Who moved my cheese bavarian bergkase cheese
+    strings fromage babybel cheese and biscuits cow emmental. Cottage cheese paneer cheese strings melted cheese fromage
+    frais edam cottage cheese who moved my cheese. Bocconcini cheddar gouda stinking bishop taleggio cow port-salut. Brie
+    manchego mascarpone. Ricotta say cheese cheese slices fondue cheesecake hard cheese cheesy feet port-salut. Lancashire
+    parmesan macaroni cheese who moved my cheese cottage cheese red leicester cottage cheese port-salut. Squirty cheese bavarian
+    bergkase cream cheese. Gouda stilton cheese strings. Ricotta queso mozzarella hard cheese lancashire when the cheese
+    comes out everybody's happy camembert de normandie cheeseburger. Cauliflower cheese who moved my cheese cottage cheese
+    jarlsberg say cheese fromage frais who moved my cheese cheesecake. Ricotta. Fromage cheese strings the big cheese. Airedale
+    the big cheese who moved my cheese red leicester rubber cheese croque monsieur the big cheese port-salut. Gouda fondue
+    bavarian bergkase ricotta swiss smelly cheese halloumi macaroni cheese. Cut the cheese swiss say cheese cottage cheese
+    red leicester macaroni cheese.</div>
+<div class="row">
+    <h2>Offsets</h2>
+</div>
+<div class="row">
+    Fondue cauliflower cheese cheese slices. Blue castello stinking bishop cheese on toast cheese triangles cheeseburger halloumi
+    halloumi danish fontina. Edam cheese on toast roquefort jarlsberg ricotta cheesy feet gouda emmental. Jarlsberg babybel
+    lancashire cheeseburger stinking bishop. Smelly cheese rubber cheese goat. Who moved my cheese bavarian bergkase cheese
+    strings fromage babybel cheese and biscuits cow emmental. Cottage cheese paneer cheese strings melted cheese fromage
+    frais edam cottage cheese who moved my cheese. Bocconcini cheddar gouda stinking bishop taleggio cow port-salut. Brie
+    manchego mascarpone. Ricotta say cheese cheese slices fondue cheesecake hard cheese cheesy feet port-salut. Lancashire
+    parmesan macaroni cheese who moved my cheese cottage cheese red leicester cottage cheese port-salut. Squirty cheese bavarian
+    bergkase cream cheese. Gouda stilton cheese strings. Ricotta queso mozzarella hard cheese lancashire when the cheese
+    comes out everybody's happy camembert de normandie cheeseburger. Cauliflower cheese who moved my cheese cottage cheese
+    jarlsberg say cheese fromage frais who moved my cheese cheesecake. Ricotta. Fromage cheese strings the big cheese. Airedale
+    the big cheese who moved my cheese red leicester rubber cheese croque monsieur the big cheese port-salut. Gouda fondue
+    bavarian bergkase ricotta swiss smelly cheese halloumi macaroni cheese. Cut the cheese swiss say cheese cottage cheese
+    red leicester macaroni cheese.</div>
